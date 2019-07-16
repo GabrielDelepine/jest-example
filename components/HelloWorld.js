c3542e709b467dec2752a606c76ea0d4851e90c9
Gabriel Delépine <gdelepine@keley-live.com>
2019-07-16 12:16:13 +0200

    simulate click on button

--- components/HelloWorld.js
+++ components/HelloWorld.js
@@ -7,13 +7,27 @@ export default class HelloWorld extends Component {
     companyName: PropTypes.string,
   }
 
+  state = {
+    buttonClicked: false,
+  }
+
+  clickButton = () => {
+    this.setState({
+      buttonClicked: true,
+    })
+  }
+
   render () {
     const { companyName } = this.props
+    const { buttonClicked } = this.state
 
     return (
       <Fragment>
         <h1>Hello World</h1>
         { companyName && <p>company: {companyName}</p> }
+        <button onClick={this.clickButton}>
+          {buttonClicked ? 'you clicked me' : 'you never clicked me'}
+        </button>
       </Fragment>
     )
   }
--- components/HelloWorld.test.js
+++ components/HelloWorld.test.js
@@ -14,3 +14,13 @@ test('companyName props', () => {
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot()
 })
+
+test('simulate click on button', () => {
+  const component = renderer.create(<HelloWorld companyName={'Ubisoft'} />)
+
+  const button = component.root.findByType('button')
+  button.props.onClick()
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot()
+})
